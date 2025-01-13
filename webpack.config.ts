6e17845e79b8ae7b40661779275a08e952d2d2f4
Ilya <iosdsbb@gmail.com>
2025-01-14 00:18:49 +0300

    chore: added comments

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,18 @@
 import * as path from "node:path";
 
 const dirname = import.meta.dirname;
-// тут undefined
+// undefined
 console.log(dirname);
 
 const config = {
     mode: "development",
 
-    // если раскомментить, то будет ошибка
-    // entry: path.resolve(dirname, "src", "index.js"),
-    //
-    // output: {
-    //     path: path.resolve(dirname, "dist"),
-    //     clean: true,
-    // }
+    entry: path.resolve(dirname, "src", "index.js"),
+
+    output: {
+        path: path.resolve(dirname, "dist"),
+        clean: true,
+    }
 };
 
 export default config;
